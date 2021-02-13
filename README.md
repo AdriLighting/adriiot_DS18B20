5e4bce901aee95d7369f87902ef7ca3256f37dbd
AdriLighting <adri0724>
2021-02-13 19:51:39 +0100

    1

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # adriiot_DS18B20
+
+<pre>
+Librairies
+<details>
+adriiot_DS18B20                 = https://github.com/AdriLighting/adriot_DS18B20
+ArduinoJson                     = https://github.com/bblanchon/ArduinoJson
+DallasTemperature               = https://github.com/milesburton/Arduino-Temperature-Control-Library
+OneWire                         = https://github.com/PaulStoffregen/OneWire
+</details>
+Dependency Graph
+<details>
+|-- [adriiot_DS18B20] 1.0.0
+|   |-- [ArduinoJson] 6.17.1
+|   |-- [DallasTemperature] 3.9.0
+|   |   |-- [OneWire] 2.3.5
+|   |-- [OneWire] 2.3.5</details>
+</pre>
--- examples/adriot_ds18b20/adriot_ds18b20.ino
+++ examples/adriot_ds18b20/adriot_ds18b20.ino
@@ -1,17 +1,17 @@
 
 #include <adriiot_DS18B20.h>
-#include <adri_tools_v2.h>
+
 DS18B20Managment		adriiot_ds18b20Management;
 DS18B20Class * _sensor;
+
 int id;
+
 void setup()
 {
 	Serial.begin(115200);
   	for(unsigned long const serialBeginTime = millis(); !Serial && (millis() - serialBeginTime > 5000); ) { }
-  	fsprintf("\n##################################################\n\n");
+  	Serial.printf("\n##################################################\n\n");
 
-
-	LittleFS.begin();	
 	adriiot_ds18b20Management.create(D1, id);
 }
 
@@ -20,6 +20,6 @@ void loop()
 	float t;
 	boolean state;
 	adriiot_ds18b20Management.module(id)->read_temperature(t, state);
-	fsprintf("\n[TEMP ] : %.2f\n", t);
+	Serial.printf("\n[TEMP ] : %.2f\n", t);
 	delay(1000);
 }
